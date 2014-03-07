a791f873ce9844870c4dff1ba05fb8cb05ba783f
dmtta <mastd863@newschool.edu>
2014-03-06 19:08:59 -0500

    Initial State

---
+++ Assets/Scripts/ballRoll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballRoll : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+				if (Input.GetKey (KeyCode.UpArrow)) {
+						GetComponent<Rigidbody> ().AddRelativeForce (transform.forward * 20f, ForceMode.Acceleration);
+				}
+				if (Input.GetKey (KeyCode.DownArrow)) {
+						GetComponent<Rigidbody> ().AddRelativeForce (-transform.forward * 20f, ForceMode.Acceleration);
+				}
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+						GetComponent<Rigidbody> ().AddRelativeForce (-transform.right * 20f, ForceMode.Acceleration);
+				}
+				if (Input.GetKey (KeyCode.RightArrow)) {
+						GetComponent<Rigidbody> ().AddRelativeForce (transform.right * 20f, ForceMode.Acceleration);
+				}
+				
+		}
+
+}
---
+++ Assets/Scripts/cubeSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if (collision.relativeVelocity.magnitude > 2) {
+						audio.Play ();
+				}
+
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
